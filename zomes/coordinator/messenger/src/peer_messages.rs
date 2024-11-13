0e787b51dbe1140bd62b4d798e1cf53165463a08
guillem.cordoba <guillem.cordoba@gmail.com>
2024-11-13 14:54:53 +0100

    Linked devices working

--- zomes/coordinator/messenger/src/peer_messages.rs
+++ zomes/coordinator/messenger/src/peer_messages.rs
@@ -3,7 +3,7 @@ use messenger_integrity::*;
 
 use crate::{
     agent_encrypted_message::{create_encrypted_message, MessengerEncryptedMessage},
-    linked_devices::get_all_agents_for,
+    linked_devices::{get_all_agents_for, query_my_linked_devices},
     signed::build_signed,
     MessengerRemoteSignal,
 };
@@ -11,6 +11,7 @@ use crate::{
 #[hdk_extern]
 pub fn send_peer_message(message_content: PeerMessage) -> ExternResult<()> {
     let agents = get_all_agents_for(message_content.recipient.clone())?;
+    let my_agents = query_my_linked_devices()?;
 
     let content = PrivateMessengerEntryContent::PeerMessage(message_content);
 
@@ -27,7 +28,19 @@ pub fn send_peer_message(message_content: PeerMessage) -> ExternResult<()> {
         agents.clone(),
     )?;
 
+    send_remote_signal(
+        MessengerRemoteSignal::NewPrivateMessengerEntry(private_messenger_entry.clone()),
+        my_agents.clone(),
+    )?;
+
     for agent in agents {
+        create_encrypted_message(
+            agent,
+            MessengerEncryptedMessage(private_messenger_entry.clone()),
+        )?;
+    }
+
+    for agent in my_agents {
         create_encrypted_message(
             agent,
             MessengerEncryptedMessage(private_messenger_entry.clone()),
