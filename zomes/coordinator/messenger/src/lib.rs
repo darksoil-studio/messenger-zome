9b32d2fe1e44e43225d9031e227c9b8002123a5c
guillem.cordoba <guillem.cordoba@gmail.com>
2025-03-19 12:13:19 +0100

    Migrate

--- zomes/coordinator/messenger/src/lib.rs
+++ zomes/coordinator/messenger/src/lib.rs
@@ -220,3 +220,27 @@ pub fn recv_remote_signal(signal_bytes: SerializedBytes) -> ExternResult<()> {
         Ok(())
     }
 }
+
+#[hdk_extern]
+pub fn migrate_from_old_cell(old_cell: CellId) -> ExternResult<()> {
+    let response = call(
+        CallTargetCell::OtherCell(old_cell),
+        zome_info()?.name,
+        "query_private_event_entries".into(),
+        None,
+        (),
+    )?;
+
+    let ZomeCallResponse::Ok(result) = response else {
+        return Err(wasm_error!(
+            "Error quering the old private event entries: {:?}.",
+            response
+        ));
+    };
+    let private_event_entries: BTreeMap<EntryHashB64, PrivateEventEntry> =
+        result.decode().map_err(|err| wasm_error!(err))?;
+
+    private_event_sourcing::import_events(private_event_entries)?;
+
+    Ok(())
+}
