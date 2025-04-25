3012fbb828551e78c4f4dd9329d50c4817dd0289
guillem.cordoba <guillem.cordoba@gmail.com>
2025-04-25 17:12:54 +0200

    WIP

--- tests/src/group-chat/events/events-consistency.test.ts
+++ tests/src/group-chat/events/events-consistency.test.ts
@@ -1,6 +1,6 @@
-import { encodeHashToBase64 } from '@holochain/client';
-import { dhtSync, pause, runScenario } from '@holochain/tryorama';
 import { toPromise } from '@darksoil-studio/holochain-signals';
+import { encodeHashToBase64 } from '@holochain/client';
+import { pause, runScenario } from '@holochain/tryorama';
 import { assert, expect, test } from 'vitest';
 
 import {
