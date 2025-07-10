61c3b9f29f265bef642dae0f2b5be676f151497d
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-07-10 14:31:36 +0000

    Deploying to gh-pages from @ darksoil-studio/messenger-zome@36ceb9e4f77f0d2e7570d785b85c20565dcc3f85 🚀

---
+++ backend/release/deps/migration_zome_trait-9b399256326bc091.d
@@ -0,0 +1,5 @@
+/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/libmigration_zome_trait-9b399256326bc091.rmeta: /home/runner/.cargo/git/checkouts/migration-zome-trait-05303447f43dff77/3878bc5/src/lib.rs
+
+/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/migration_zome_trait-9b399256326bc091.d: /home/runner/.cargo/git/checkouts/migration-zome-trait-05303447f43dff77/3878bc5/src/lib.rs
+
+/home/runner/.cargo/git/checkouts/migration-zome-trait-05303447f43dff77/3878bc5/src/lib.rs:
