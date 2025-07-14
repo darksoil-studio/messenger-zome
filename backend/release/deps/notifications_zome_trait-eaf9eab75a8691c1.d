57e2de42cd58d5d44d13c23825842aab881a1577
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-07-14 10:33:41 +0000

    Deploying to gh-pages from @ darksoil-studio/messenger-zome@fa6f07fa8299ae1a62424cf433851186cf8ffd97 🚀

--- backend/release/deps/notifications_zome_trait-eaf9eab75a8691c1.d
+++ backend/release/deps/notifications_zome_trait-eaf9eab75a8691c1.d
@@ -1,5 +1,5 @@
-/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/libnotifications_zome_trait-eaf9eab75a8691c1.rmeta: /home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/c90799b/src/lib.rs
+/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/libnotifications_zome_trait-eaf9eab75a8691c1.rmeta: /home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/ff949ab/src/lib.rs
 
-/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/notifications_zome_trait-eaf9eab75a8691c1.d: /home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/c90799b/src/lib.rs
+/home/runner/work/messenger-zome/messenger-zome/docs/public/backend/release/deps/notifications_zome_trait-eaf9eab75a8691c1.d: /home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/ff949ab/src/lib.rs
 
-/home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/c90799b/src/lib.rs:
+/home/runner/.cargo/git/checkouts/notifications-zome-trait-fcca6e624485b9ac/ff949ab/src/lib.rs:
